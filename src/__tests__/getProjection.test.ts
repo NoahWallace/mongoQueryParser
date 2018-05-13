3a7e8508690ae853845465fcf0218e3c7fe8404e
noahwallace <noah.z.wallace@gmail.com>
2018-05-13 12:54:45 -0400

    added tests

--- src/__tests__/getProjection.test.ts
+++ src/__tests__/getProjection.test.ts
@@ -1,13 +1,63 @@
-import * as chai from "chai";
-const should = chai.should();
 import { getProjection } from "../parseProjectionString";
 
 
 describe("#getProjection",()=>{
+    it("should exclude _id",(done)=>{
+        let projection=getProjection("_id");
+        projection.should.have.property("_id").that.equals(0);
+        done();
+    });
+    it("should include name",(done)=>{
+        let projection=getProjection("name");
+        projection.should.have.property("name").that.equals(1);
+        done();
+    });
+    it("should include multiple simple projections",(done)=>{
+        let projection=getProjection("name,title,description");
+        projection.should.have.property("name").that.equals(1);
+        projection.should.have.property("title").that.equals(1);
+        projection.should.have.property("description").that.equals(1);
+        done();
+    });
     it("should return an elemMatch parsed projection",(done)=>{
-        let str = "name contains 'object eq 'abc' {AND} score eq 23',_id";
+        let str = "subdoc contains 'title eq 'abc'";
         let parsedProjection=getProjection(str);
-        // console.log(JSON.stringify(parsedProjection));
+        parsedProjection.should.have.property("_id").that.equals(0);
+        parsedProjection.should.have.property("subdoc").is.an("object");
+        parsedProjection["subdoc"]
+            .should.have.property("$elemMatch")
+            .that.is.an("object")
+            .that.has.property("title")
+            .that.is.an("object")
+            .that.has.property("$eq")
+            .that.equals("abc");
+        done();
+    })
+    it("should return an elemMatch parsed projection",(done)=>{
+        let str = "subdoc contains 'title eq 'abc' {AND} score eq 23',_id";
+        let parsedProjection=getProjection(str);
+        // {"_id":0,"subdoc":{"$elemMatch":{"$and":[{"title":{"$eq":"abc"}},{"score":{"$eq":23}}]}}}
+        parsedProjection.should.have.property("_id").that.equals(0);
+        parsedProjection.should.have.property("subdoc").is.an("object");
+        parsedProjection["subdoc"]
+            .should.have.property("$elemMatch")
+            .that.is.an("object")
+            .that.has.property("$and")
+            .that.is.an("array");
+        done();
+    })
+
+    it("should return an elemMatch parsed projection",(done)=>{
+        let str = "subdoc contains 'title eq 'abc' {OR} score eq 23',_id";
+        let parsedProjection=getProjection(str);
+        // {"_id":0,"subdoc":{"$elemMatch":{"$and":[{"title":{"$eq":"abc"}},{"score":{"$eq":23}}]}}}
+        parsedProjection.should.have.property("_id").that.equals(0);
+        parsedProjection.should.have.property("subdoc").is.an("object");
+        parsedProjection["subdoc"]
+            .should.have.property("$elemMatch")
+            .that.is.an("object")
+            .that.has.property("$or")
+            .that.is.an("array");
         done();
     })
 })
